5c2f60018d9fa5cbf9029f2a287c83a10ddb9146
Alexander Shubin <alexander.shubin@geogracom.com>
2020-04-09 20:57:15 +0300

    Добавил логгер и возможность просматривать фронт без DevTools

--- src/store.js
+++ src/store.js
@@ -1,23 +1,37 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import thunk from "redux-thunk";
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import rootReducer from "@/_reducers";
+import {createLogger} from 'redux-logger';
+
+const logger = createLogger({
+    /* https://github.com/evgenyrodionov/redux-logger */
+    collapsed: true,
+    diff: true
+});
 
 const initalState = {};
-const middleware = [thunk];
+const middleware = [thunk, logger];
 
 let store;
 
 if (window.navigator.userAgent.includes("Chrome")) {
-  store = createStore(
-    rootReducer,
-    initalState,
-    compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-  );
+    store = createStore(
+        rootReducer,
+        initalState,
+        composeWithDevTools(
+            applyMiddleware(...middleware)
+        )
+    );
 } else {
-  store = createStore(rootReducer, initalState, compose(applyMiddleware(...middleware)));
+    store = createStore(
+        rootReducer,
+        initalState,
+        compose
+        (applyMiddleware(...middleware)));
 }
 
 //временно, потом удалить
-window.store = store;
+//window.store = store;
 
 export default store;
