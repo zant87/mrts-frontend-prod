aa75c5307bafe56691e957b7a994b09d2e4db6ed
Alexander Shubin <alexander.shubin@geogracom.com>
2020-04-30 02:37:11 +0300

    Поправил Swagger.jsx

--- src/Common/Swagger.jsx
+++ src/Common/Swagger.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import {MDBContainer, MDBIframe} from 'mdbreact';
+import {MDBCol, MDBContainer, MDBIframe, MDBRow, MDBSpinner} from 'mdbreact';
+import MUIDataTable from "mui-datatables";
 
 class SwaggerPage extends React.Component {
 
     render() {
         return (
-            <MDBContainer fluid>
-                <MDBIframe src="http://localhost:8080/swagger-ui/index.html"/>
-            </MDBContainer>
-        );
+        <MDBContainer fluid>
+            <MDBRow center>
+                <MDBCol md={'12'} className='my-5 mx-auto'>
+                    <MDBIframe src="http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config"/>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
+        )
     }
 }
 
