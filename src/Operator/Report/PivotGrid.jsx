4b5841a27a011c70ae2549050d0ba3d3196360a3
Alexander Shubin <alexander.shubin@geogracom.com>
2020-06-03 16:57:47 +0300

    Пример PivotGrid.jsx

--- src/Operator/Report/PivotGrid.jsx
+++ src/Operator/Report/PivotGrid.jsx
@@ -6,28 +6,99 @@ import PivotGrid, {
 import PivotGridDataSource from 'devextreme/ui/pivot_grid/data_source';
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
-import {MDBContainer, MDBRow} from "mdbreact";
-import {sales} from "./pivot";
+import {MDBCol, MDBContainer, MDBRow, MDBSpinner} from "mdbreact";
+import appAxios from "../../_services/appAxios";
 
 export default class OperatorReportPivotGridPage extends React.Component {
 
+    state = {
+        isLoading: true,
+        dataSource: {}
+    };
+
+    componentDidMount() {
+        this.getData();
+    };
+
+    getData = () => {
+        this.setState({isLoading: true});
+        appAxios.get(`/views/k-5-s?sort=id,desc&size=5000`)
+            .then(res => {
+                console.log(res);
+                // const count = Number(res.headers['x-total-count']);
+                const data = res.data;
+                this.setState({
+                    data: data, isLoading: false, dataSource:
+                        new PivotGridDataSource({
+                            fields: [
+                                // {
+                                //     caption: 'Источник данных',
+                                //     width: 120,
+                                //     dataField: 'dataProviderName',
+                                //     area: 'row'
+                                // },
+                                // {
+                                //     caption: 'Вид транспорта',
+                                //     width: 120,
+                                //     dataField: 'transportTypeName',
+                                //     area: 'row'
+                                // },
+                                // {
+                                //     caption: 'Форма',
+                                //     width: 120,
+                                //     dataField: 'formCode',
+                                //     area: 'row'
+                                // },
+                                {
+                                    caption: 'Показатель',
+                                    dataField: 'parameterName',
+                                    width: 150,
+                                    area: 'row'
+                                },
+                                {
+                                    caption: 'Отчетный год',
+                                    dataField: 'year',
+                                    dataType: 'number',
+                                    area: 'column'
+                                },
+                                {
+                                    caption: 'Отчетный квартал',
+                                    dataField: 'quarterName',
+                                    area: 'column'
+                                },
+                                {
+                                    caption: 'Значение показателя',
+                                    dataField: 'value',
+                                    dataType: 'number',
+                                    summaryType: 'sum',
+                                    area: 'data'
+                                }
+                            ],
+                            store: data
+                        })
+                });
+            });
+    };
+
     render() {
+
+        const {dataSource, isLoading} = this.state;
 
         return (
             <MDBContainer fluid>
                 <MDBRow center className='my-5'>
+                    {isLoading && <MDBSpinner multicolor/>}
                     <PivotGrid
                         id="sales"
                         dataSource={dataSource}
-                        allowSortingBySummary={true}
+                        // allowSortingBySummary={true}
                         allowSorting={true}
                         allowFiltering={true}
                         allowExpandAll={true}
-                        height={440}
-                        showBorders={true}
-                    >
-                        <Export enabled={true} fileName="Sales"/>
-                        <FieldChooser enabled={false}/>
+                        // height={100}
+                        showBorders={true}>
+                        <Export enabled={true} fileName="Фактические значения показателей"/>
+                        <FieldChooser enabled={true}/>
                     </PivotGrid>
                 </MDBRow>
             </MDBContainer>
@@ -35,31 +106,3 @@ export default class OperatorReportPivotGridPage extends React.Component {
     }
 }
 
-const dataSource = new PivotGridDataSource({
-    fields: [{
-        caption: 'Region',
-        width: 120,
-        dataField: 'region',
-        area: 'row'
-    }, {
-        caption: 'City',
-        dataField: 'city',
-        width: 150,
-        area: 'row',
-        selector: function (data) {
-            return `${data.city} (${data.country})`;
-        }
-    }, {
-        dataField: 'date',
-        dataType: 'date',
-        area: 'column'
-    }, {
-        caption: 'Sales',
-        dataField: 'amount',
-        dataType: 'number',
-        summaryType: 'sum',
-        format: 'currency',
-        area: 'data'
-    }],
-    store: sales
-});
