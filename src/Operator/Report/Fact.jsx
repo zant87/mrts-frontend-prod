ddcb6a1f0cdeb385e29f69c3375ff2026a8185f5
Alexander Shubin <alexander.shubin@geogracom.com>
2020-06-05 06:59:12 +0300

    Тест фильтрации

--- src/Operator/Report/Fact.jsx
+++ src/Operator/Report/Fact.jsx
@@ -9,21 +9,77 @@ export default class OperatorReportFactPage extends React.Component {
         page: 0,
         count: 0,
         data: [],
-        isLoading: false
+        isLoading: false,
+        dataProviderList: [],
+        transportTypeList: [],
+        parameterList: [],
+    }
+
+    componentDidMount() {
+        this.getDataProviderList();
+        this.getTransportTypeList();
+        this.getParameterList();
+    };
+
+    getParameterList = async () => {
+        this.setState({isLoading: true});
+        appAxios.get(`/parameters`)
+            .then(res => {
+                // console.log(res);
+                const data = res.data.map(item => {
+                    return item.name;
+                })
+                this.setState({parameterList: data, isLoading: false});
+            });
+    }
+
+    getTransportTypeList = async () => {
+        this.setState({isLoading: true});
+        appAxios.get(`/nsi-transport-types`)
+            .then(res => {
+                const data = res.data.map(item => {
+                    return {id: item.id, name: item.name};
+                })
+                const mod_data = data.reduce(function (acc, cur, i) {
+                    acc[cur.id] = cur.name;
+                    return acc;
+                }, {});
+                console.log(data);
+                console.log(mod_data);
+                this.setState({transportTypeList: mod_data, isLoading: false});
+            });
+    }
+
+
+    getDataProviderList = async () => {
+        this.setState({isLoading: true});
+        appAxios.get(`/nsi-data-providers`)
+            .then(res => {
+                const data = res.data.map(item => {
+                    return item.name;
+                })
+                this.setState({dataProviderList: data, isLoading: false});
+            });
     }
 
     render() {
 
         const columns = [
-            {field: 'id', title: '#'},
+            {field: 'id', title: '#', filtering: false},
             {field: 'dataProviderName', title: 'Источник данных', editable: 'never'},
-            {field: 'transportTypeName', title: 'Вид транспорта', editable: 'never'},
-            {field: 'formCode', title: 'Форма', editable: 'never'},
+            // {field: 'transportTypeName', title: 'Вид транспорта', editable: 'never'},
+            {
+                field: 'transportTypeId',
+                title: 'Вид транспорта',
+                lookup: this.state.transportTypeList,
+                editable: 'never'
+            },
+            {field: 'formCode', title: 'Форма', editable: 'never', filtering: false},
             {field: 'parameterName', title: 'Показатель', editable: 'never'},
             {field: 'year', title: 'Отчетный год', editable: 'never'},
             {field: 'quarterName', title: 'Отчетный квартал', editable: 'never'},
             {field: 'okeiName', title: 'Единица измерения', editable: 'never'},
-            {field: 'value', title: 'Значение показателя'},
+            {field: 'value', title: 'Значение показателя', filtering: false},
         ];
 
         const tableRef = React.createRef();
@@ -61,6 +117,8 @@ export default class OperatorReportFactPage extends React.Component {
                                             const index = oldData.tableData.id;
                                             dataUpdate[index] = newData;
 
+                                            console.log(newData);
+
                                             const requestData = {id: newData.id, value: newData.value};
 
                                             appAxios({
@@ -82,7 +140,8 @@ export default class OperatorReportFactPage extends React.Component {
                                 actionsColumnIndex: 999,
                                 search: false,
                                 pageSize: 20,
-                                pageSizeOptions: [20, 50, 100]
+                                pageSizeOptions: [20, 50, 100],
+                                filtering: true
                             }}
                         />
                     </MDBCol>
