1df3f9851f2bdd7c6b0b50e6fac87f3fd6b3ea25
Alexander Shubin <alexander.shubin@geogracom.com>
2020-06-08 18:58:48 +0300

    Пример карты

--- src/Analyst/Map.jsx
+++ src/Analyst/Map.jsx
@@ -1,20 +1,86 @@
 import React from 'react';
-import {MDBBreadcrumb, MDBBreadcrumbItem, MDBContainer, MDBRow} from "mdbreact";
+import {loadModules} from 'esri-loader';
+import config from 'config';
 
-const AnalystMapPage = () => {
-    return (
-        <MDBContainer>
-            <MDBRow className='mt-5'>
-                <MDBBreadcrumb>
-                    <MDBBreadcrumbItem>Главная</MDBBreadcrumbItem>
-                    <MDBBreadcrumbItem active>Карта мероприятий</MDBBreadcrumbItem>
-                </MDBBreadcrumb>
-            </MDBRow>
-            <MDBRow>
-                <h1>Карта мероприятий</h1>
-            </MDBRow>
-        </MDBContainer>
-    );
-};
+class AnalystMapPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.mapRef = React.createRef();
+    }
+
+    componentDidMount() {
+        loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/layers/MapImageLayer', 'esri/widgets/Legend'], {css: true})
+            .then(([ArcGISMap, MapView, FeatureLayer, MapImageLayer, Legend]) => {
+
+                const template = {
+                    title: "{NAME}",
+                    content: [
+                        {
+                            type: "fields",
+                            fieldInfos: [
+                                {
+                                    fieldName: "PERCENT_READY",
+                                    label: "Уровень технической готовности к 31.12.2018 г.",
+                                },
+                                {
+                                    fieldName: "COST_PLAN ",
+                                    label: "Планируемый бюджет, млрд руб."
+                                },
+                            ]
+                        }
+                    ]
+                };
+
+                const layer = new MapImageLayer({
+                    url: config.arcGisMapServerUrl,
+                    sublayers: [
+                        {
+                            id: 26,
+                        },
+                        {
+                            id: 1,
+                            popupEnabled: true,
+                            popupTemplate: template
+                        }]
+                });
+
+                const map = new ArcGISMap({
+                    layers: [layer]
+                });
+
+                this.view = new MapView({
+                    container: this.mapRef.current,
+                    map: map,
+                    center: [37, 55],
+                    zoom: 12,
+                });
+
+                const legend = new Legend({
+                    view: this.view,
+                    layerInfos: [
+                        {
+                            layer: layer,
+                            title: "МРТС"
+                        }
+                    ]
+                });
+
+                this.view.ui.add(legend, "bottom-right");
+
+            });
+    }
+
+    componentWillUnmount() {
+        if (this.view) {
+            this.view.container = null;
+        }
+    }
+
+    render() {
+        return (
+            <div className="webmap my-5" ref={this.mapRef}/>
+        );
+    }
+}
 
 export default AnalystMapPage;
--- webpack.dev.js
+++ webpack.dev.js
@@ -100,7 +100,8 @@ const config = {
     // глобальный конфиг
     config: JSON.stringify({
       apiUrl: "http://localhost:4000",
-      isLocalDeployment: true
+      isLocalDeployment: true,
+      arcGisMapServerUrl: "http://srvdev.geogracom.com:6080/arcgis/rest/services/TS_projects/MapServer"
     }),
   },
 };
