4f97532fa8f2a78de0162ab7d7be56cb917f9890
Alexander Shubin <alexander.shubin@geogracom.com>
2020-06-19 03:04:48 +0300

    Рыба для загрузки файлов

--- src/Admin/Loading/FromXLSX.jsx
+++ src/Admin/Loading/FromXLSX.jsx
@@ -1,21 +1,57 @@
 import React from 'react';
-import {MDBBreadcrumb, MDBBreadcrumbItem, MDBCol, MDBContainer, MDBRow} from "mdbreact";
+import {MDBContainer, MDBRow, MDBFileInput} from "mdbreact";
+import MDBFileupload from 'mdb-react-fileupload';
 
-const AdminLoadingFromXLSXPage = () => {
-    return (
-        <MDBContainer>
-            <MDBRow className='mt-5'>
-                <MDBBreadcrumb>
-                    <MDBBreadcrumbItem>Главная</MDBBreadcrumbItem>
-                    <MDBBreadcrumbItem>Загрузка</MDBBreadcrumbItem>
-                    <MDBBreadcrumbItem active>Загрузка из XLSX</MDBBreadcrumbItem>
-                </MDBBreadcrumb>
-            </MDBRow>
-            <MDBRow>
-                <h1>Загрузка из XLSX</h1>
-            </MDBRow>
-        </MDBContainer>
-    );
+export default class AdminLoadingFromXLSXPage extends React.Component {
+
+    state = {
+        selectedFile: null,
+    }
+
+    onSubmitHandler = e => {
+
+        console.log(e);
+
+        e.preventDefault();
+
+        let data = new FormData();
+        data.append('file', this.state.selectedFile);
+
+        console.log('submit', data)
+
+        // this.fileupload.resetPreview();
+    }
+
+    fileInputHandler = e => {
+        console.log(e);
+    }
+
+    render() {
+        return (
+            <MDBContainer>
+                <MDBRow center>
+                    <form>
+                        <MDBFileupload
+                            getValue={this.fileInputHandler}
+                            maxFileSize="5M"
+                            showRemove
+                            showErrors
+                            showSubmitButton
+                            messageDefault='Перетащите файл для загрузки или нажмите сюда'
+                            minWidth='300'
+                            messageError='Ошибка при загрузке файла'
+                            messageRemove='Удалить'
+                            messageReplace='Заменить загруженный файл'
+                            messageSubmit='Отправить'
+                            onSubmitHandler={this.onSubmitHandler}
+                            ref={fileupload => this.fileupload = fileupload}
+                        />
+                    </form>
+                </MDBRow>
+                <MDBRow center>
+                    <MDBFileInput btnColor="default" getValue={this.fileInputHandler}/>
+                </MDBRow>
+            </MDBContainer>
+        );
+    }
 };
-
-export default AdminLoadingFromXLSXPage;
