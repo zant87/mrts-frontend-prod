bfaf6f7e69ef2ac363af4b95b51b66a96644f068
Alexander Shubin <alexander.shubin@geogracom.com>
2020-07-14 01:41:55 +0300

    Закрыл MRTS-495 (серверная пагицания и фильтрация)

--- src/Admin/Structure/Formulas.jsx
+++ src/Admin/Structure/Formulas.jsx
@@ -8,39 +8,17 @@ import {history} from "@/_helpers";
 
 export default class AdminStructureFormulasPage extends React.Component {
 
-    state = {
-        data: [],
-        isLoading: true
-    };
-
-    getIndicators = () => appAxios.get(`indicators`).catch(err => null);
-
     async componentDidMount() {
-        try {
-            const [rIndicators] = await Axios.all([this.getIndicators()]);
-
-            this.setState(
-                {
-                    data: rIndicators.data,
-                    isLoading: false,
-
-                }
-            );
-        } catch (err) {
-            console.log(err.message);
-        }
     }
 
     render() {
 
         const columns = [
             {field: 'id', title: '#', filtering: false, editable: 'never'},
-            {field: 'transportTypeName', title: 'Строковое значение', editable: 'never'},
-            {field: 'name', title: 'Наименование', editable: 'never'},
-            // {field: 'formula', title: 'Формула', editable: 'never'},
+            {field: 'transportTypeName', filtering: true, title: 'Вид транспорта', editable: 'never'},
+            {field: 'name', title: 'Наименование', filtering: true, editable: 'never'},
         ];
 
-        const {data, isLoading} = this.state;
         const tableRef = React.createRef();
 
         return (
@@ -51,15 +29,46 @@ export default class AdminStructureFormulasPage extends React.Component {
                             title="Формулы расчета индикаторов"
                             columns={columns}
                             tableRef={tableRef}
-                            data={data}
-                            isLoading={isLoading}
+                            data={query =>
+                                new Promise((resolve, reject) => {
+
+                                        let url = `/indicators-page?page=${query.page}&size=${query.pageSize}`;
+
+                                        if (query.orderBy) {
+                                            url += `&sort=${query.orderBy.field},${query.orderDirection}`;
+                                        }
+
+                                        if (query.filters.length > 0) {
+                                            query.filters.forEach(element => {
+                                                if (element.value.length > 3)
+                                                    url += `&${element.column.field}.contains=${element.value}`;
+                                            });
+                                        }
+
+                                        console.log(url);
+
+                                        appAxios.get(url)
+                                            .then(res => {
+                                                const count = Number(res.headers['x-total-count']);
+                                                const data = res.data;
+
+                                                resolve({
+                                                    data: data,
+                                                    page: query.page,
+                                                    totalCount: count
+                                                });
+                                            });
+
+                                    }
+                                )
+                            }
                             tableLayout={'fixed'}
                             localization={ruLocalization}
                             options={{
-                                search: true,
                                 pageSize: 20,
                                 pageSizeOptions: [20, 50, 100],
                                 actionsColumnIndex: 999,
+                                filtering: true
                             }}
                             actions={[
                                 {
