f0ca9e83bd858c2073556563a619267c2b6cf859
Aleksandr Shubin <alexander.shubin@geogracom.com>
2020-08-30 20:10:43 +0300

    Закрыл MRTS-5

---
+++ src/Admin/Loading/ResourcesFromXLSX.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {MDBContainer, MDBRow, MDBFileInput, MDBBtn, toast, MDBCol, MDBSpinner} from "mdbreact";
+import appAxios from "../../_services/appAxios";
+import {authenticationService} from "../../_services";
+
+export default class AdminLoadingResourcesFromXLSXPage extends React.Component {
+
+    state = {
+        file: null,
+        isLoading: false,
+        user: null
+    }
+
+    getUser = (id) => appAxios.get(`users?username.equals=${id}`)
+        .then(response => {
+            console.log('User = ', response.data[0]);
+            this.setState({user: response.data[0]});
+        })
+        .catch(err => null);
+
+    componentDidMount() {
+        this.getUser(authenticationService.currentUserValue.id);
+    }
+
+    fileInputHandler = e => {
+        console.log(e[0]);
+        this.setState({file: e[0]});
+    }
+
+    fileUpload = async () => {
+
+        console.log(this.state.file);
+        console.log('Грузим файл!');
+
+        const formData = new FormData();
+        formData.append('file', this.state.file);
+        formData.append('username', this.state.user.username);
+
+        appAxios({
+            url: `/budget-batches-upload`,
+            method: 'POST',
+            data: formData
+        }).then((response) => {
+            toast.success(`Синхронизация с ID ${response.data} запущена`, {closeButton: false});
+        }).catch(function (error) {
+            console.log(error);
+            toast.error(`Ошибка загрузки XLSX файла`, {closeButton: false});
+        });
+
+    }
+
+    render() {
+
+        console.log(this.state);
+
+        return (
+            <MDBContainer>
+                <MDBRow center>
+                    <h2>Загрузка ресурсного обеспечения</h2>
+                </MDBRow>
+                <MDBRow center>
+                    <MDBCol>
+                        <MDBFileInput btnColor="success"
+                                      reset
+                                      getValue={this.fileInputHandler}
+                                      textFieldTitle='Файл для загрузки...'
+                                      btnTitle='Выберите файл'/>
+                    </MDBCol>
+                </MDBRow>
+                <MDBBtn color="success" onClick={this.fileUpload}>Загрузка</MDBBtn>
+                {/*<MDBRow center>*/}
+                {/*    <MDBCol middle={true}>{this.state.isLoading && <MDBSpinner multicolor small={true}/>}</MDBCol>*/}
+                {/*    <MDBCol></MDBCol>*/}
+                {/*</MDBRow>*/}
+            </MDBContainer>
+        );
+    }
+};
