4adf6b868e5d4c7984770fcd5ffb9e8aa638e18a
Aleksandr Shubin <alexander.shubin@geogracom.com>
2020-09-01 12:36:05 +0300

    Вернул старую карточку для проектов в карте

--- src/Analyst/ArcgisConfig/ProjectsTemplate.js
+++ src/Analyst/ArcgisConfig/ProjectsTemplate.js
@@ -1,53 +1,46 @@
 export const ProjectsTemplate = {
-
     title: '{NAME}',
-    content: (feature) => {
-        const div = document.createElement("div");
-        div.innerHTML = feature.graphic.attributes.NAME + "<br>" +
-            feature.graphic.attributes.CODE + "<br>" +
-            "<button>Нажми меня</button>";
-        return div;
-    },
-    fieldInfos: [
+    content: [
         {
-            fieldName: 'NAME',
-            label: 'Название проекта'
-        },
-        {
-            fieldName: 'CODE',
-            label: 'Код проекта'
-        },
-        {
-            fieldName: 'PERCENT_READY',
-            label: 'Уровень технической готовности к 31.12.2018 г.',
-        },
-        {
-            fieldName: 'COST_PLAN',
-            label: 'Планируемый бюджет, млрд руб.'
-        },
-        {
-            fieldName: 'YEAR_START',
-            label: ' Год начала проекта'
-        },
-        {
-            fieldName: 'YEAR_FINISH',
-            label: 'Год завершения проекта'
-        },
-        {
-            fieldName: 'VARIANT',
-            label: 'Вариант Стратегии'
-        },
-        {
-            fieldName: 'TYPE_BUDGET',
-            label: 'Источник финансирования'
-        },
-        {
-            fieldName: 'PROJECT_TYPE',
-            label: 'Вид транспорта'
-        },
-        {
-            fieldName: 'WORK_STAGE',
-            label: 'Этапы'
-        },
-    ],
+            type: 'fields',
+            fieldInfos: [
+                {
+                    fieldName: 'NAME',
+                    label: 'Название проекта'
+                },
+                {
+                    fieldName: 'PERCENT_READY',
+                    label: 'Уровень технической готовности к 31.12.2018 г.',
+                },
+                {
+                    fieldName: 'COST_PLAN',
+                    label: 'Планируемый бюджет, млрд руб.'
+                },
+                {
+                    fieldName: 'YEAR_START',
+                    label: ' Год начала проекта'
+                },
+                {
+                    fieldName: 'YEAR_FINISH',
+                    label: 'Год завершения проекта'
+                },
+                {
+                    fieldName: 'VARIANT',
+                    label: 'Вариант Стратегии'
+                },
+                {
+                    fieldName: 'TYPE_BUDGET',
+                    label: 'Источник финансирования'
+                },
+                {
+                    fieldName: 'PROJECT_TYPE',
+                    label: 'Вид транспорта'
+                },
+                {
+                    fieldName: 'WORK_STAGE',
+                    label: 'Этапы'
+                }
+            ]
+        }
+    ]
 };
