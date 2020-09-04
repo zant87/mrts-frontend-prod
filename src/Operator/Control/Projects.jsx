abdde6696656e2bbf4eb82bc3b557d83dc2610c3
Zant <zemtsov@mintrans.org>
2020-09-04 19:18:35 +0300

    Исправлены ошибки в ссылках к бэку на страницах Контроль крупных инвестиционных проектов и контроль ресурсного обеспечения

--- src/Operator/Control/Projects.jsx
+++ src/Operator/Control/Projects.jsx
@@ -32,7 +32,7 @@ export default class OperatorControlProjectsPage extends React.Component {
                 <TableContainer
                     columns={columns}
                     title={'Контроль поступления и согласования данных по выполнению крупных инвестиционных проектов'}
-                    baseUrl={'views/control-project-reportsList'}
+                    baseUrl={'views/control-project-reports'}
                     filtersList={filtersList}
                     filterMinimalLength={filterMinimalLength}
                     loadAll={true}
