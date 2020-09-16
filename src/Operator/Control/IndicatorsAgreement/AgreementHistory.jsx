2117f3a9cc1e2c416126d60bea341702ca7e35d9
Aleksandr Shubin <alexander.shubin@geogracom.com>
2020-09-16 15:51:47 +0300

    Добавил историю согласований в бэк и фронт

---
+++ src/Operator/Control/IndicatorsAgreement/AgreementHistory.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import appAxios from "../../../_services/appAxios";
+import Axios from "axios";
+import {ruLocalization} from "../../../_components";
+import {toast} from "mdbreact";
+import MaterialTable from "material-table";
+import TableContainer from "../../../Containers/TableContainer";
+import construct from "@babel/runtime/helpers/esm/construct";
+
+export default class AgreementHistoryPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        console.log('AgreementHistoryPage got props =', props);
+    }
+
+    render() {
+
+        const columns = [
+            {field: 'id', title: '#'},
+            {field: 'fullname', title: 'Пользователь'},
+            {field: 'beginDate', title: 'Дата начала действия согласования', type: 'date'},
+            {field: 'endDate', title: 'Дата окончания действия согласования', type: 'date'},
+            {field: 'state', title: 'Состояние'},
+        ];
+
+        const filtersList = {
+            'beginDate': 'date',
+            'endDate': 'date',
+        };
+
+        return (
+            <TableContainer
+                columns={columns}
+                title={''}
+                filtersList={filtersList}
+                filterMinimalLength={1}
+                baseUrl={`views/agreement-histories?document.equals=${this.props.document}&user=${this.props.username}`}
+                modifiedBaseUrl={true}
+                loadAll={true}
+            />
+        );
+    }
+}
