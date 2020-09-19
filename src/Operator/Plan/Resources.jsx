563e0d76eedbee354dd9c7ebf1707bb83688f6b1
Ant Zemtsov <zemtsov@mintrans.org>
2020-09-19 19:45:12 +0400

    Исправил отображение версии ТС на странице План - Крупные инвестиционные проекты

--- src/Operator/Plan/Resources.jsx
+++ src/Operator/Plan/Resources.jsx
@@ -1,82 +1,75 @@
-import React from 'react';
-import {MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, toast} from "mdbreact";
+import React from "react";
+import { MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, toast } from "mdbreact";
 import TableContainer from "../../Containers/TableContainer";
 import ResourcesEdit from "./ResourcesEdit";
 
 export default class OperatorPlanResourcesPage extends React.Component {
+  state = {
+    modal: false,
+    row: {},
+    action: "",
+    initialized: true,
+  };
 
-    state = {
-        modal: false,
-        row: {},
-        action: '',
-        initialized: true
+  tableRef = React.createRef();
+
+  toggleModal = (rowData, action) => {
+    if (rowData && action) {
+      this.setState({
+        modal: !this.state.modal,
+        row: rowData,
+        action: action,
+      });
+    } else {
+      this.setState({
+        modal: !this.state.modal,
+      });
     }
+  };
 
-    tableRef = React.createRef();
+  render() {
+    const columns = [
+      { field: "id", title: "#", filtering: false },
+      // {field: 'transportStrategyCode', title: 'Редакция ТС'},
+      { field: "transportStrategyVersionId", title: "Редакция ТС" },
+      { field: "scenarioName", title: "Вариант реализации стратегии" },
+      { field: "costTypeName", title: "Вид вложений" },
+      { field: "directionName", title: "Направление вложений" },
+      { field: "fundingSourceName", title: "Источник финансирования" },
+      { field: "stageName", title: "Период реализации стратегии", filtering: false },
+      { field: "planingMin", title: "Минимальное ресурсное обеспечение, млрд. руб.", filtering: false },
+      { field: "planingMax", title: "Максимальное ресурсное обеспечение, млрд. руб.", filtering: false },
+    ];
 
-    toggleModal = (rowData, action) => {
-        if (rowData && action) {
-            this.setState({
-                modal: !this.state.modal,
-                row: rowData,
-                action: action
-            });
-        } else {
-            this.setState({
-                modal: !this.state.modal,
-            });
-        }
-    }
+    const actions = [
+      {
+        icon: "edit",
+        tooltip: "Редактировать",
+        onClick: (event, rowData) => {
+          if (this.state.initialized) this.toggleModal(rowData, "edit");
+        },
+      },
+    ];
 
-
-    render() {
-
-        const columns = [
-            {field: 'id', title: '#', filtering: false},
-            {field: 'transportStrategyCode', title: 'Редакция ТС'},
-            {field: 'scenarioName', title: 'Вариант реализации стратегии'},
-            {field: 'costTypeName', title: 'Вид вложений'},
-            {field: 'directionName', title: 'Направление вложений'},
-            {field: 'fundingSourceName', title: 'Источник финансирования'},
-            {field: 'stageName', title: 'Период реализации стратегии', filtering: false},
-            {field: 'planingMin', title: 'Минимальное ресурсное обеспечение, млрд. руб.', filtering: false},
-            {field: 'planingMax', title: 'Максимальное ресурсное обеспечение, млрд. руб.', filtering: false},
-        ];
-
-        const actions = [
-            {
-                icon: 'edit',
-                tooltip: 'Редактировать',
-                onClick: (event, rowData) => {
-                    if (this.state.initialized) this.toggleModal(rowData, 'edit');
-                }
-            }
-        ];
-
-        return (
-            <React.Fragment>
-                <TableContainer
-                    columns={columns}
-                    title={'Ресурсное обеспечение ТС (план)'}
-                    baseUrl={'views/k-4-s'}
-                    actions={actions}
-                    tableRef={this.tableRef}
-                    loadAll={true}
-                />
-                <MDBContainer>
-                    <MDBModal isOpen={this.state.modal} toggle={this.toggleModal} backdrop={false} size="lg">
-                        <MDBModalHeader toggle={this.toggleModal}>Форма редактирования</MDBModalHeader>
-                        <MDBModalBody>
-                            <ResourcesEdit
-                                data={this.state.row}
-                                tableRef={this.tableRef}
-                                toggleModal={this.toggleModal}
-                            />
-                        </MDBModalBody>
-                    </MDBModal>
-                </MDBContainer>
-            </React.Fragment>
-
-        )
-    }
-};
+    return (
+      <React.Fragment>
+        <TableContainer
+          columns={columns}
+          title={"Ресурсное обеспечение ТС (план)"}
+          baseUrl={"views/k-4-s"}
+          actions={actions}
+          tableRef={this.tableRef}
+          loadAll={true}
+        />
+        <MDBContainer>
+          <MDBModal isOpen={this.state.modal} toggle={this.toggleModal} backdrop={false} size="lg">
+            <MDBModalHeader toggle={this.toggleModal}>Форма редактирования</MDBModalHeader>
+            <MDBModalBody>
+              <ResourcesEdit data={this.state.row} tableRef={this.tableRef} toggleModal={this.toggleModal} />
+            </MDBModalBody>
+          </MDBModal>
+        </MDBContainer>
+      </React.Fragment>
+    );
+  }
+}
