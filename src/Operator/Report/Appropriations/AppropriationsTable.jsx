d184332d8bc90a57c26e8348612b6cdc37bffc07
Aleksandr Shubin <alexander.shubin@geogracom.com>
2020-09-21 19:05:40 +0300

    Сделал АРМ Оператор - Отчет - Ресурсное обеспечение

--- src/Operator/Report/Appropriations/AppropriationsTable.jsx
+++ src/Operator/Report/Appropriations/AppropriationsTable.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {MDBCol, MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, MDBRow, toast} from "mdbreact";
 import TableContainer from "../../../Containers/TableContainer";
 import AppropriationsEdit from "./AppropriationsEdit";
+import NumericFilter from "../../../Common/Filters/NumericFilter";
+import StringFilter from "../../../Common/Filters/StringFilter";
+import TableContainerWithFilters from "../../../Containers/TableContainerWithFilters";
 
 export default class OperatorReportAppropriationsPage extends React.Component {
 
@@ -9,19 +12,51 @@ export default class OperatorReportAppropriationsPage extends React.Component {
         modal: false,
         row: {},
         action: '',
-        initialized: true
-    };
+        initialized: true,
+        filtersList: {
+            year: {
+                type: "integer",
+                operator: "equals",
+                value: null
+            },
+            directionName: {
+                type: "text",
+                operator: "contains",
+                value: null
+            },
+            fundingName: {
+                type: "text",
+                operator: "contains",
+                value: null
+            },
+            costTypeName: {
+                type: "text",
+                operator: "contains",
+                value: null
+            },
+            plan: {
+                type: "numeric",
+                operator: "equals",
+                value: null
+            },
+            fact: {
+                type: "numeric",
+                operator: "equals",
+                value: null
+            },
+        }
+    }
 
     tableRef = React.createRef();
 
     toggleModal = (rowData, action) => {
         console.log(rowData);
         if (rowData && action) {
-        this.setState({
-            modal: !this.state.modal,
-            row: rowData,
-            action: action
-        });
+            this.setState({
+                modal: !this.state.modal,
+                row: rowData,
+                action: action
+            });
         } else {
             this.setState({
                 modal: !this.state.modal,
@@ -29,15 +64,65 @@ export default class OperatorReportAppropriationsPage extends React.Component {
         }
     }
 
+    updateFilter = (e) => {
+        console.log('Update Filter received =', e);
+        let newFilter = this.state.filtersList;
+        newFilter[e.id] = {value: e.value, operator: e.operator, type: e.type};
+        console.log('New Filter =', newFilter);
+        this.setState({filtersList: newFilter});
+    }
+
     render() {
 
         const columns = [
-            {field: 'year', title: 'Отчетный год'},
-            {field: 'directionName', title: 'Направление расходов'},
-            {field: 'fundingName', title: 'Источник финансирования'},
-            {field: 'costTypeName', title: 'Вид расходов'},
-            {field: 'plan', title: 'Запланировано, млн. руб.'},
-            {field: 'fact', title: 'Кассовое исполнение, млн. руб.'},
+            {
+                field: 'year', title: 'Отчетный год', filtering: true,
+                filterComponent: props => {
+                    return <NumericFilter id={props.columnDef.field} columnId={props.columnDef.tableData.id}
+                                          filter={this.state.filtersList[props.columnDef.field]}
+                                          filterChanged={props.onFilterChanged} changed={this.updateFilter}/>;
+                }
+            },
+            {
+                field: 'directionName', title: 'Направление расходов', filtering: true,
+                filterComponent: props => {
+                    return <StringFilter id={props.columnDef.field} columnId={props.columnDef.tableData.id}
+                                         filter={this.state.filtersList[props.columnDef.field]}
+                                         filterChanged={props.onFilterChanged} changed={this.updateFilter}/>;
+                },
+            },
+            {
+                field: 'fundingName', title: 'Источник финансирования', filtering: true,
+                filterComponent: props => {
+                    return <StringFilter id={props.columnDef.field} columnId={props.columnDef.tableData.id}
+                                         filter={this.state.filtersList[props.columnDef.field]}
+                                         filterChanged={props.onFilterChanged} changed={this.updateFilter}/>;
+                },
+            },
+            {
+                field: 'costTypeName', title: 'Вид расходов', filtering: true,
+                filterComponent: props => {
+                    return <StringFilter id={props.columnDef.field} columnId={props.columnDef.tableData.id}
+                                         filter={this.state.filtersList[props.columnDef.field]}
+                                         filterChanged={props.onFilterChanged} changed={this.updateFilter}/>;
+                }
+            },
+            {
+                field: 'plan', title: 'Запланировано, млн. руб.', filtering: true,
+                filterComponent: props => {
+                    return <NumericFilter id={props.columnDef.field} columnId={props.columnDef.tableData.id}
+                                          filter={this.state.filtersList[props.columnDef.field]}
+                                          filterChanged={props.onFilterChanged} changed={this.updateFilter}/>;
+                }
+            },
+            {
+                field: 'fact', title: 'Кассовое исполнение, млн. руб.', filtering: true,
+                filterComponent: props => {
+                    return <NumericFilter id={props.columnDef.field} columnId={props.columnDef.tableData.id}
+                                          filter={this.state.filtersList[props.columnDef.field]}
+                                          filterChanged={props.onFilterChanged} changed={this.updateFilter}/>;
+                }
+            },
         ];
 
         const actions = [
@@ -50,20 +135,14 @@ export default class OperatorReportAppropriationsPage extends React.Component {
             },
         ];
 
-        const filtersList = {
-            'year': 'numeric',
-            'plan': 'numeric',
-            'fact': 'numeric',
-        };
-
         return (
             <React.Fragment>
-                <TableContainer
+                <TableContainerWithFilters
                     columns={columns}
                     title={'Ресурсное обеспечение'}
                     baseUrl={'views/k-9-s'}
                     actions={actions}
-                    filtersList={filtersList}
+                    filtersList={this.state.filtersList}
                     tableRef={this.tableRef}
                     loadAll={true}
                 />
