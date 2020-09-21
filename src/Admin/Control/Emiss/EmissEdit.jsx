c88e1ca4fe841d6a38ed73c10baff5ea0cfcc13a
Anton Zemtsov <zemtsov@mintrans.org>
2020-09-21 21:45:08 +0300

    Добавил кнопки сохранить и закрыть в АДМ-контроль-исполнители и контактные данные

--- src/Admin/Control/Emiss/EmissEdit.jsx
+++ src/Admin/Control/Emiss/EmissEdit.jsx
@@ -1,90 +1,106 @@
 import React from "react";
-import {MDBBtn, MDBContainer, MDBRow, toast} from "mdbreact";
+import { MDBBtn, MDBContainer, MDBRow, toast } from "mdbreact";
 import appAxios from "../../../_services/appAxios";
-import {useForm, Controller} from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 
-const AdminControlEmissEditPage = props => {
+const AdminControlEmissEditPage = (props) => {
+  const { handleSubmit, errors, control } = useForm();
 
-    const {handleSubmit, errors, control} = useForm();
+  const onSubmit = (data, close) => {
+    console.log("Submitting Data =", data);
 
-    const onSubmit = data => {
-
-        console.log('Submitting Data =', data);
-
-        if (props.action === 'add') {
-            appAxios({
-                url: `emiss-forms`,
-                method: 'POST',
-                data: data
-            }).then((response) => {
-                const message = response.headers["x-mrts-backend-params"];
-                toast.success(`Успешно добавлена запись с ID ${message}`, {
-                    closeButton: false
-                });
-                props.toggleModal();
-                props.tableRef.current.onQueryChange();
-            });
-        } else {
-            appAxios({
-                url: `emiss-forms`,
-                method: 'PUT',
-                data: data
-            }).then((response) => {
-                const message = response.headers["x-mrts-backend-params"];
-                toast.success(`Успешно обновлена запись с ID ${message}`, {
-                    closeButton: false
-                });
-                props.toggleModal();
-                props.tableRef.current.onQueryChange();
-            });
+    if (props.action === "add") {
+      appAxios({
+        url: `emiss-forms`,
+        method: "POST",
+        data: data,
+      }).then((response) => {
+        const message = response.headers["x-mrts-backend-params"];
+        toast.success(`Успешно добавлена запись с ID ${message}`, {
+          closeButton: false,
+        });
+        if (close) {
+          props.toggleModal();
         }
+        props.tableRef.current.onQueryChange();
+      });
+    } else {
+      appAxios({
+        url: `emiss-forms`,
+        method: "PUT",
+        data: data,
+      }).then((response) => {
+        const message = response.headers["x-mrts-backend-params"];
+        toast.success(`Успешно обновлена запись с ID ${message}`, {
+          closeButton: false,
+        });
+        if (close) {
+          props.toggleModal();
+        }
+        props.tableRef.current.onQueryChange();
+      });
     }
+  };
 
-    return (
-        <div className="scrollbar my-1 mx-auto" style={{maxHeight: '85vh'}}>
-            <React.Fragment>
-                <MDBContainer>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <MDBRow center>
-                            <FormControl fullWidth={true} className='mx-auto'>
-                                <Controller as={<TextField disabled className='mb-3'/>}
-                                            name="id"
-                                            label="#"
-                                            control={control}
-                                            defaultValue={props.data ? props.data.id : ''}/>
-                                <Controller as={<TextField className='mb-3'/>}
-                                            name="name"
-                                            label="Наименование формы"
-                                            control={control}
-                                            defaultValue={props.data ? props.data.name : ''}/>
-                                <Controller as={<TextField className='mb-3'/>}
-                                            name="responsible"
-                                            label="Ответственный"
-                                            control={control}
-                                            defaultValue={props.data ? props.data.responsible : ''}/>
-                                <Controller as={<TextField className='mb-3'/>}
-                                            name="email"
-                                            label="Электронная почта"
-                                            control={control}
-                                            defaultValue={props.data ? props.data.email : ''}/>
-                                <Controller as={<TextField className='mb-3'/>}
-                                            name="phone"
-                                            label="Телефон"
-                                            control={control}
-                                            defaultValue={props.data ? props.data.phone : ''}/>
-                            </FormControl>
-                        </MDBRow>
-                        <MDBRow center className='my-3'>
-                            <MDBBtn color="indigo" size="sm" type='submit'>Применить</MDBBtn>
-                        </MDBRow>
-                    </form>
-                </MDBContainer>
-            </React.Fragment>
-
-        </div>
-    );
-}
+  return (
+    <div className="scrollbar my-1 mx-auto" style={{ maxHeight: "85vh" }}>
+      <React.Fragment>
+        <MDBContainer>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <MDBRow center>
+              <FormControl fullWidth={true} className="mx-auto">
+                <Controller
+                  as={<TextField disabled className="mb-3" />}
+                  name="id"
+                  label="#"
+                  control={control}
+                  defaultValue={props.data ? props.data.id : ""}
+                />
+                <Controller
+                  as={<TextField className="mb-3" />}
+                  name="name"
+                  label="Наименование формы"
+                  control={control}
+                  defaultValue={props.data ? props.data.name : ""}
+                />
+                <Controller
+                  as={<TextField className="mb-3" />}
+                  name="responsible"
+                  label="Ответственный"
+                  control={control}
+                  defaultValue={props.data ? props.data.responsible : ""}
+                />
+                <Controller
+                  as={<TextField className="mb-3" />}
+                  name="email"
+                  label="Электронная почта"
+                  control={control}
+                  defaultValue={props.data ? props.data.email : ""}
+                />
+                <Controller
+                  as={<TextField className="mb-3" />}
+                  name="phone"
+                  label="Телефон"
+                  control={control}
+                  defaultValue={props.data ? props.data.phone : ""}
+                />
+              </FormControl>
+            </MDBRow>
+            <MDBRow center className="my-3">
+              <MDBBtn color="primary" type="submit" onClick={handleSubmit((e) => onSubmit(e, false))}>
+                Сохранить
+              </MDBBtn>
+              <MDBBtn color="primary" type="submit" onClick={handleSubmit((e) => onSubmit(e, true))}>
+                Сохранить и закрыть
+              </MDBBtn>
+            </MDBRow>
+          </form>
+        </MDBContainer>
+      </React.Fragment>
+    </div>
+  );
+};
 
 export default AdminControlEmissEditPage;
--- src/Admin/Control/Executors/ExecutorsEdit.jsx
+++ src/Admin/Control/Executors/ExecutorsEdit.jsx
@@ -1,81 +1,79 @@
 import React from "react";
-import {MDBBtn, MDBContainer, MDBRow, toast} from "mdbreact";
+import { MDBBtn, MDBContainer, MDBRow, toast } from "mdbreact";
 import appAxios from "../../../_services/appAxios";
-import {useForm, Controller} from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 
-const AdminControlExecutorsEditPage = props => {
+const AdminControlExecutorsEditPage = (props) => {
+  const { handleSubmit, errors, control } = useForm();
 
-    const {handleSubmit, errors, control} = useForm();
+  const onSubmit = (data, close) => {
+    console.log("Submitting Data =", data);
+    appAxios({
+      url: `users`,
+      method: "PUT",
+      data: data,
+    }).then((response) => {
+      const message = response.headers["x-mrts-backend-params"];
+      toast.success(`Успешно обновлена запись с ID ${message}`, {
+        closeButton: false,
+      });
+      if (close) {
+        props.toggleModal();
+      }
+      props.tableRef.current.onQueryChange();
+    });
+  };
 
-    const onSubmit = data => {
-        console.log('Submitting Data =', data);
-        appAxios({
-            url: `users`,
-            method: 'PUT',
-            data: data
-        }).then((response) => {
-            const message = response.headers["x-mrts-backend-params"];
-            toast.success(`Успешно обновлена запись с ID ${message}`, {
-                closeButton: false
-            });
-            props.toggleModal();
-            props.tableRef.current.onQueryChange();
-        });
-    }
-
-    return (
-        <div className="scrollbar my-1 mx-auto" style={{maxHeight: '85vh'}}>
-            <React.Fragment>
-                <MDBContainer>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        {props.data ?
-                            <MDBRow center>
-                                <FormControl fullWidth={true} className='mx-auto'>
-                                    <Controller as={<TextField disabled className='mb-3'/>}
-                                                name="id"
-                                                label="#"
-                                                control={control}
-                                                defaultValue={props.data.id}/>
-                                    <Controller as={<TextField className='mb-3'/>}
-                                                name="email"
-                                                label="Электронная почта"
-                                                control={control}
-                                                defaultValue={props.data.email}/>
-                                    <Controller as={<TextField className='mb-3'/>}
-                                                name="fullname"
-                                                label="ФИО"
-                                                control={control}
-                                                defaultValue={props.data.fullname}/>
-                                    <Controller as={<TextField className='mb-3'/>}
-                                                name="phone"
-                                                label="Телефон"
-                                                control={control}
-                                                defaultValue={props.data.phone}/>
-                                    <Controller as={<TextField className='mb-3'/>}
-                                                name="department"
-                                                label="Департамент"
-                                                control={control}
-                                                defaultValue={props.data.department}/>
-                                    <Controller as={<TextField disabled className='mb-3'/>}
-                                                name="username"
-                                                label="ID пользователя"
-                                                control={control}
-                                                defaultValue={props.data.username}/>
-                                </FormControl>
-                            </MDBRow>
-                            : null
-                        }
-                        <MDBRow center className='my-3'>
-                            <MDBBtn color="indigo" size="sm" type='submit'>Применить</MDBBtn>
-                        </MDBRow>
-                    </form>
-                </MDBContainer>
-            </React.Fragment>
-
-        </div>
-    );
-}
+  return (
+    <div className="scrollbar my-1 mx-auto" style={{ maxHeight: "85vh" }}>
+      <React.Fragment>
+        <MDBContainer>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {props.data ? (
+              <MDBRow center>
+                <FormControl fullWidth={true} className="mx-auto">
+                  <Controller as={<TextField disabled className="mb-3" />} name="id" label="#" control={control} defaultValue={props.data.id} />
+                  <Controller
+                    as={<TextField className="mb-3" />}
+                    name="email"
+                    label="Электронная почта"
+                    control={control}
+                    defaultValue={props.data.email}
+                  />
+                  <Controller as={<TextField className="mb-3" />} name="fullname" label="ФИО" control={control} defaultValue={props.data.fullname} />
+                  <Controller as={<TextField className="mb-3" />} name="phone" label="Телефон" control={control} defaultValue={props.data.phone} />
+                  <Controller
+                    as={<TextField className="mb-3" />}
+                    name="department"
+                    label="Департамент"
+                    control={control}
+                    defaultValue={props.data.department}
+                  />
+                  <Controller
+                    as={<TextField disabled className="mb-3" />}
+                    name="username"
+                    label="ID пользователя"
+                    control={control}
+                    defaultValue={props.data.username}
+                  />
+                </FormControl>
+              </MDBRow>
+            ) : null}
+            <MDBRow center className="my-3">
+              <MDBBtn color="primary" type="submit" onClick={handleSubmit((e) => onSubmit(e, false))}>
+                Сохранить
+              </MDBBtn>
+              <MDBBtn color="primary" type="submit" onClick={handleSubmit((e) => onSubmit(e, true))}>
+                Сохранить и закрыть
+              </MDBBtn>
+            </MDBRow>
+          </form>
+        </MDBContainer>
+      </React.Fragment>
+    </div>
+  );
+};
 
 export default AdminControlExecutorsEditPage;
